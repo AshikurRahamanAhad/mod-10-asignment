f590f69d2f71cbda423137ba5075404aabd0f9d8
MD Ashikur Rahaman Ahad <aragamingevolution@gmail.com>
2025-12-19 16:54:42 +0600

    function-2

--- test.js
+++ test.js
@@ -1,6 +1,21 @@
-
+/* 1. Function Name: calculateSum */
 function calculateSum(a, b) { 
     return a + b;
 }
 calculateSum(2, 3);
-console.log("calculatedSum");
+console.log(calculateSum(2, 3));
+
+/* 2. Function Name: isEven */
+function isEven(numb) { 
+    if (numb % 2 === 0) {
+        return true;
+    } 
+    else {
+        return false;
+    }
+}
+isEven(10);
+console.log(isEven(10));
+
+
+
